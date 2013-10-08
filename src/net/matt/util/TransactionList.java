fce2f40acd1d4ba477b28fe241911cbd3a18b86f
navx2810 <navx2810@gmail.com>
2013-10-08 19:27:52 -0400

    Updated transactionList

--- src/net/matt/util/TransactionList.java
+++ src/net/matt/util/TransactionList.java
@@ -1,5 +1,11 @@
 package net.matt.util;
 
+/** This class will keep track of the player's transactions
+ * @author Matt
+ *
+ */
 public class TransactionList {
 
+	String code, description;
+	double price;
 }
